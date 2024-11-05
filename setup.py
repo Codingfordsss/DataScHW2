e33b35d88a537b09dc29675df6c0ff8cf7b62d29
Codingfordsss <150239133+Codingfordsss@users.noreply.github.com>
2024-11-05 22:59:48 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "math_quiz=math_quiz.math_quiz:math_quiz_game"
+        ]
+    },
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A simple math quiz command-line game.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="<link-to-your-repository>",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
